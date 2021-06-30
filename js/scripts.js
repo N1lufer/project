43497cc6ac691c2ef4de0b5024d60f7ad7822926
Nilufer <mamedli_nl@mail.ru>
2021-06-30 22:14:19 +0300

    project 30.06

--- js/scripts.js
+++ js/scripts.js
@@ -16,17 +16,31 @@ $(function(){
     
     $('button.basket').click(function(){
         $('.popup-desk').addClass('active');
+        $('.popup').html('<p class="popup-header">Личный кабинет</p><input type="text" name="fullname" placeholder="Логин"><input type="password" name="password" placeholder="Пароль"><button type="submit">Войти</button><a href="https://yandex.ru/" class="register">Зарегистрироваться</a>');
+        $('.basket').html(6);
     });
     
     $('.popup-desk').click(function(e){
         if (e.target == this) {
             $(this).removeClass('active');
+            $('.popup').empty();
+            $('.basket').html(5);
         }
     });
     
-    $('.register').click(function(e){
+    $(document).on('click', '.register', function(e){
         e.preventDefault();
-        
-        $('.popup').html('<p class="popup-header">Личный кабинет закрыт на ремонт.<br>Регистрации не будет до 1 января.</p>')
+        if ($('.basket').html()==6) {
+            $('.popup').html('<p class="popup-header">Личный кабинет закрыт на ремонт.<br>Регистрации не будет до 1 января.</p><a href="https://yandex.ru/" class="register">Войти</a>');
+            $('.basket').html(5);
+        } else {
+            $('.popup').html('<p class="popup-header">Личный кабинет</p><input type="text" name="fullname" placeholder="Логин"><input type="password" name="password" placeholder="Пароль"><button type="submit">Войти</button><a href="https://yandex.ru/" class="register">Зарегистрироваться</a>');
+            $('.basket').html(6);
+        }
     });
+    
+    $(document).on('click', 'button[type="submit"]', function(){
+        alert('OGOGO!!!')
+    });
+    
 });
