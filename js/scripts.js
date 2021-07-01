d225719323db04b78fd3daa01a06cb97ac2d4373
Nilufer <mamedli_nl@mail.ru>
2021-07-01 14:54:03 +0300

    project 01.07

--- js/scripts.js
+++ js/scripts.js
@@ -40,7 +40,67 @@ $(function(){
     });
     
     $(document).on('click', 'button[type="submit"]', function(){
-        alert('OGOGO!!!')
+        alert('OGOGO!!!');
     });
     
+    $(document).on('click', '.order .del > div', function(){
+        tovarDelete(this);
+    });
+    
+    $(document).on('change input', '.order .num > input', function(){
+        tovarChange(this);
+    });
 });
+
+
+
+/* order */
+const order = [
+    {
+        id: 5711,
+        name: 'Товар 1',
+        value: 10,
+        price: 5
+    },
+    {
+        id: 3432,
+        name: 'Товар 2',
+        value: 10,
+        price: 10
+    },
+    {
+        id: 4846,
+        name: 'Товар 3',
+        value: 10,
+        price: 15
+    }
+]
+function tovarDelete(point) {
+    let b = point.parentNode.parentNode;
+    let t_id = b.querySelector('th').dataset.tovar;
+    for (let i = 0; i < order.length; i++) {
+        if (order[i].id == t_id) {
+            order.splice(i, 1);
+            console.log(t_id); // имитация отправки бэку сообщения об удалении товара
+            break;
+        }
+    }
+    b.remove();
+    if (order.length > 0) {
+        tovarCount();
+    } else {
+        document.querySelector('.table').remove();
+        document.querySelector('.nihil').style = 'display:block';
+    }
+}
+function tovarChange() {
+    // опознать товар
+    // найти его в ордере
+    // изменить количество в ордере
+    // вызвать пересчет +
+    
+    tovarCount();
+}
+function tovarCount() {
+    alert('It works!');
+}
